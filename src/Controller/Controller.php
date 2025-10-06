6d17a89ce0180ce88dbb162fa3ea8dfd34839876
Jaksa <ciglar.7@gmail.com>
2025-10-06 22:52:18 +0200

    Refactor files to twig

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -2,33 +2,52 @@
 namespace App\Controller;
 include_once "Page.php";
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class Controller
+class Controller extends AbstractController
 {
-    #const $page;
+    function __constructor(){
+    }
     
+    #const $page;
     #new PageStructure();
 
     #[Route('/')]
     public function index(): Response
     {
-        return new Response(new Page()->indexPage());
+        return $this->render("default/index.html.twig");
+        //return new Response(new Page()->indexPage());
     }
 
     #[Route('/newMatch')]
     public function newMatch(): Response
     {
 
-        return new Response(new Page()->newMatchPage());
+        return $this->render("default/newMatch.html.twig");
     }
 
     #[Route('/scoreSummary')]
     public function scoreSummary(): Response
     {
 
-        return new Response(new Page()->scoreSummaryPage());
+        return $this->render("default/scoreSummary.html.twig");
     }
+
+    /*public function new(Request $request): Response
+    {
+        // createFormBuilder is a shortcut to get the "form factory"
+        // and then call "createBuilder()" on it
+
+        $form = $this->createFormBuilder()
+            ->add('task', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->getForm();
+
+        return $this->render('task/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }*/
 }
 ?>
