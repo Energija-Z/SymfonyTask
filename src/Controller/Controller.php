116e547d89108e4292ca805625e9266a422606b7
Jaksa <ciglar.7@gmail.com>
2025-10-09 21:12:05 +0200

    Add score listing, finializing application

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 use App\Entity\ScoreForm;
 use App\Form\Type\TeamFormType;
@@ -15,31 +17,30 @@ use App\Service\SharedDataService;
 
 class Controller extends AbstractController
 {
+    /**
+     * Homepage with links.
+     */
     #[Route('/', name: 'homepage')]
-    public function index(SharedDataService $sds): Response
+    public function index(): Response
     {
-        foreach($sds->getScores() as $i)
-            echo $i." score";
         return $this->render("default/index.html.twig");
     }
 
-    #[Route('/scoreSummary/{homeTeam},{awayTeam},{homeTeamScore},{awayTeamScore}', name: "scoreSummary")]
-    public function scoreSummary(string $homeTeam, string $awayTeam, int $homeTeamScore, int $awayTeamScore, SharedDataService $sds): Response
+    /**
+     * Page that lists the teams and their scores.
+     */
+    #[Route('/scoreSummary', name: "scoreSummary")]
+    public function scoreSummary(SessionInterface $session): Response
     {
-        $var = new ScoreForm($homeTeam, $awayTeam, $homeTeamScore, $awayTeamScore);
-        $sds->setScore($var);
-        
-        foreach($sds->getScores() as $i)
-            echo $i->getAwayTeam()." score";
-
         return $this->render("default/scoreSummary.html.twig", [
-            "homeTeam" => $homeTeam,
-            "homeTeamScore" => $homeTeamScore,
-            "awayTeam" => $awayTeam,
-            "awayTeamScore" => $awayTeamScore
+            "data" => $session->get("match"),
         ]);
     }
 
+    /**
+     * Form to write team names.
+     * It navigates to current match page.
+     */
     #[Route('/newMatch', name: "newMatch")]
     public function newMatch(Request $request): Response
     {
@@ -60,8 +61,12 @@ class Controller extends AbstractController
         ]);
     }
 
+    /**
+     * Form to set score for teams. User can also let the page be, as the scores can increment themselves through a JS script, until the button is pressed.
+     * It navigates to score summary page.
+     */
     #[Route('/currentMatch/{homeTeam},{awayTeam}', name: 'currentMatch')]
-    public function currentMatch(string $homeTeam, string $awayTeam, Request $request): Response
+    public function currentMatch(string $homeTeam, string $awayTeam, Request $request, SessionInterface $session): Response
     {
         $form = $this->createForm(ScoreFormType::class);
         $form->get("homeTeamScore")->setData(0);
@@ -71,12 +76,30 @@ class Controller extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {
             $task = $form->getData();
 
-            return $this->redirectToRoute('scoreSummary', [
-                "homeTeam" => $homeTeam,
-                "awayTeam" => $awayTeam,
-                "homeTeamScore" => $task["homeTeamScore"],
-                "awayTeamScore" => $task["awayTeamScore"]
-            ]);
+            $cursor = 1;
+            $homeTeamScore = $task["homeTeamScore"];
+            $awayTeamScore = $task["awayTeamScore"];
+
+            $newMatch = new ScoreForm($homeTeam, $awayTeam, $homeTeamScore, $awayTeamScore);
+
+            if($session->get("match")){
+                $matchArray = $session->get("match");
+                array_unshift($matchArray, $newMatch);
+
+                $a = usort($matchArray, function($previous, $next){
+                    $totalPreviousScore = $previous->getHomeTeamScore() + $previous->getAwayTeamScore();
+                    $totalNextScore = $next->getHomeTeamScore() + $next->getAwayTeamScore();
+                    return $totalPreviousScore <
+                    
+                    $totalNextScore ? 1 : -1;
+                });
+
+                $session->set("match", $matchArray);
+            }
+            else
+                $session->set("match", array($newMatch));
+
+            return $this->redirectToRoute('scoreSummary');
         }
 
         return $this->render('default/currentMatch.html.twig', [
